3e87234ec669699f4e21baf1b9ef1921f8b3e482
Sakthiiii <87663226+Sakthiiii@users.noreply.github.com>
2021-08-05 12:27:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var request=new XMLHttpRequest ();
+request .open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send ();
+request.onload =function (){
+     var data =JSON.parse(this.response);
+     console.log(data);
+      var rdata = data.filter((ele)=>ele.region==='Asia');
+      console.log (rdata);
+
+      var rdata1=data.filter((ele)=>ele.population<200000);
+      console.log(rdata1);
+
+      var rdata2=data.reduce((acc,ele)=>acc+ele.population,0);
+      console.log(rdata2);
+
+
+var rdata3 =data.filter((ele)=>{
+      for(let i in ele.currencies){
+            if (ele.currencies[i].code==="USD"){
+                  return true;
+            }
+      }
+});
+console.log(rdata3);
+}
